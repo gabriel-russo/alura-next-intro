7dbe066f007d7afd1c536e7ab9005d97cf2a255b
Gabriel Russo <gabrielrusso@protonmail.com>
2024-11-07 16:33:07 -0400

    Capítulo 4 - Paginando dados

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import {CardPost} from "@/components/CardPost";
 import logger from "@/logger";
+import Link from "next/link";
+import {CardPost} from "@/components/CardPost";
 import {CardPostGrid} from "@/components/CardPostGrid";
+import styles from "./page.module.css";
 
-const getAllPosts = async () => {
-  const response = await fetch('http://localhost:3042/posts');
+const getAllPosts = async (page) => {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
   if (!response.ok) {
     logger.error('Ops, alguma coisa ocorreu mal!');
     return [];
@@ -12,16 +14,25 @@ const getAllPosts = async () => {
   return response.json();
 }
 
-export default async function Home() {
-  const posts = await getAllPosts();
-
+export default async function Home({searchParams}) {
+  const currentPage = searchParams?.page || 1;
+  const {data: posts, prev, next} = await getAllPosts(currentPage);
   return (
-    <main>
+    <main className={styles.container}>
       <CardPostGrid>
         {posts.map(post => (
-          <CardPost post={post}/>
+          <CardPost key={post.id} post={post}/>
         ))}
       </CardPostGrid>
+      <div className={styles.pagination}>
+        {prev &&
+          <Link className={styles.pagination__link} href={`/?page=${prev}`}>Página anterior</Link>
+        }
+        <br/>
+        {next &&
+          <Link className={styles.pagination__link} href={`/?page=${next}`}>Próxima página</Link>
+        }
+      </div>
     </main>
   );
 };
